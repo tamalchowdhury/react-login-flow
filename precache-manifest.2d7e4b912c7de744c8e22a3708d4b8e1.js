df7a2092bbe827a6b3d0ec39d86353adc068f3e4
Tamal Anwar <tamalanwaronline@gmail.com>
2020-01-02 12:52:50 +0600

    Updates

---
+++ precache-manifest.2d7e4b912c7de744c8e22a3708d4b8e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13a0a856fa9b860127199abc93a140e",
+    "url": "/react-login-flow/index.html"
+  },
+  {
+    "revision": "15cc6a38b80309c396ac",
+    "url": "/react-login-flow/static/css/main.41c49110.chunk.css"
+  },
+  {
+    "revision": "b006c53de562eb6d6462",
+    "url": "/react-login-flow/static/js/2.0d3440b2.chunk.js"
+  },
+  {
+    "revision": "15cc6a38b80309c396ac",
+    "url": "/react-login-flow/static/js/main.7605bcda.chunk.js"
+  },
+  {
+    "revision": "d19ea882b252a08db83c",
+    "url": "/react-login-flow/static/js/runtime-main.83ee8b6b.js"
+  }
+]);
