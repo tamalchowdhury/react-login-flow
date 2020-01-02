302be605cfbd9007876695ae02ac77f5b53d9277
Tamal Anwar <tamalanwaronline@gmail.com>
2020-01-02 12:31:29 +0600

    Fixed className in bars

--- src/Bars.jsx
+++ src/Bars.jsx
@@ -4,17 +4,17 @@ import './dash-bar.css';
 class Bars extends React.Component {
   render() {
     return (
-      <div class="igFrameBar">
-        <div class="igData igData1"></div>
-        <div class="igData igData2"></div>
-        <div class="igData igData3"></div>
-        <div class="igData igData4"></div>
-        <div class="igData igData5"></div>
-        <div class="igData igData6"></div>
-        <div class="igData igData7"></div>
-        <div class="igData igData8"></div>
-        <div class="igData igData9"></div>
-        <div class="igData igData10"></div>
+      <div className="igFrameBar">
+        <div className="igData igData1"></div>
+        <div className="igData igData2"></div>
+        <div className="igData igData3"></div>
+        <div className="igData igData4"></div>
+        <div className="igData igData5"></div>
+        <div className="igData igData6"></div>
+        <div className="igData igData7"></div>
+        <div className="igData igData8"></div>
+        <div className="igData igData9"></div>
+        <div className="igData igData10"></div>
       </div>
     );
   }
